8aa03cf77851fff07c7ea6d580e3be7ebe43700e
Phuong Tran <tranphiphuong2763@gmail.com>
2023-06-17 20:30:45 +0700

    feat: implement detail user page

--- libs/core/src/lib/environments/environment.development.ts
+++ libs/core/src/lib/environments/environment.development.ts
@@ -3,7 +3,7 @@ export const environment = {
     debug: true,
     production: false,
     GOOGLE_MAPS_API_KEY: 'AIzaSyAGbKyV6NLLj3GoKWuHRyS03CZph5L9Huw',
-    // apiURL: 'https://cwp-m63p.onrender.com/api',
+    apiURL: 'https://cwp-m63p.onrender.com/api',
     webView: 'http://localhost:3000/',
-    apiURL: 'http://localhost:4000/api'
+    // apiURL: 'http://localhost:4000/api'
 };
--- libs/remotes-app/client/src/lib/modules/community/page/user-profile/user-profile.component.ts
+++ libs/remotes-app/client/src/lib/modules/community/page/user-profile/user-profile.component.ts
@@ -1,8 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { environment } from '@cwp/core/endpoint';
+import { UserProfileModel } from '@cwp/core/model/response';
+import { PostService } from '@cwp/core/services';
 
 @Component({
   selector: 'cwp-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss'],
 })
-export class UserProfileComponent {}
+export class UserProfileComponent implements OnInit {
+  posts: any[] = [];
+  userId!: string;
+  userProfile: UserProfileModel = {
+    id: '',
+    email: '123@gmail.com',
+    userAvatarUrl: 'https://via.placeholder.com/300',
+    role: '',
+    isFinishedTutorial: false
+  }
+
+  constructor(
+    private router: ActivatedRoute,
+    private postService: PostService
+  ) {}
+
+  ngOnInit(): void {
+    this.router.params.subscribe((params) => {
+      this.userId = params['userId'];
+      if (this.userId) {
+        this.getUserProfile();
+        this.getPosts();
+      }
+    });
+    // TODO: remove when integrate api
+    this.getPosts();
+  }
+
+  getUserProfile() {
+    // TODO: get user profile
+  }
+
+  getPosts() {
+    this.postService.getTrendingPosts(1).subscribe((res) => {
+      this.posts = res.data;
+    });
+  }
+
+  openPreview(url: string): void {
+    window.open(environment.webView + url, '_blank');
+  }
+}
