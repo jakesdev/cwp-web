04aa44e6c84f0a340ef79d5f1b1a8a63f7653299
linh <linh>
2023-06-18 15:52:48 +0700

    clean css

--- apps/design-system/src/app/app.component.ts
+++ apps/design-system/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cwp-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'design-system';
