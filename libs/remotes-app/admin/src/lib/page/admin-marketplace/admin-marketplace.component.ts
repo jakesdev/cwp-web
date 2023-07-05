45438d949f911538cd5a49d51f881dcad76d20a7
Linh Nguyen <linhnlh2001@gmail.com>
2023-07-05 07:56:49 +0700

    feat: add get transactions in admin marketplace

--- libs/remotes-app/admin/src/lib/page/admin-marketplace/admin-marketplace.component.ts
+++ libs/remotes-app/admin/src/lib/page/admin-marketplace/admin-marketplace.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TransactionService } from '@cwp/core/services';
 
 @Component({
   selector: 'cwp-admin-marketplace',
   templateUrl: './admin-marketplace.component.html',
   styleUrls: ['./admin-marketplace.component.scss'],
 })
-export class AdminMarketplaceComponent {}
+export class AdminMarketplaceComponent implements OnInit {
+  transactions!: any[];
+
+  constructor(private readonly transactionService: TransactionService) {}
+
+  ngOnInit(): void {
+    this.transactionService.getTransactionList().subscribe((res) => {
+      this.transactions = [...res.data];
+      console.log(this.transactions);
+    });
+  }
+}
