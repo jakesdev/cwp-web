251deaef3ce0a716d7860be6340c394c99ffb2f0
Linh Nguyen <linh.hoang.nguyen@enlabsoftware.com>
2023-07-21 16:58:04 +0700

    update portal

--- apps/portal/src/app/app.component.ts
+++ apps/portal/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'cwp-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Creative Widget';
+  constructor(
+    private meta: Meta,
+    private title: Title) {
+    this.meta.addTags([
+      { name: 'description', content: 'Web site created using create-angular-app' },
+      { name: 'author', content: 'Creative Web Platform' },
+      { name: 'keywords', content: 'Angular, Angular 11, Angular Universal, Angular CLI, Web Builder, Creative Web Platform' },
+    ]);
+    this.setTitle('Creative Web Builder Platform');
+  }
+  public setTitle(newTitle: string) {
+    this.title.setTitle(newTitle);
+  }
 }
